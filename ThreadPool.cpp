dc511cbd6b3da7a63ad99f30a7b4e063c31093ad
Kostya <vitashapo@gmail.com>
2026-02-20 19:50:29 +0300

    updated enque method with std::forward, std::shared_ptr

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -5,6 +5,10 @@
 #include <mutex>
 #include <condition_variable>
 #include <vector>
+#include <future>
+#include <memory>
+
+std::mutex global_cout_mutex;
 
 class SimplePool
 {
@@ -13,7 +17,6 @@ class SimplePool
     std::mutex mtx;
     std::condition_variable cv;
     bool done = false;
-    static inline std::mutex cout_mtx;
     public:
     SimplePool(int n)
     {
@@ -42,12 +45,12 @@ class SimplePool
                     }
 
                     {
-                        std::lock_guard<std::mutex> cout_lock(cout_mtx);
+                        std::lock_guard<std::mutex> cout_lock(global_cout_mutex);
                         std::cout << i << " thread executing the task!" << std::endl;
                     }
                     task();
                     {
-                        std::lock_guard<std::mutex> cout_lock(cout_mtx);
+                        std::lock_guard<std::mutex> cout_lock(global_cout_mutex);
                         std::cout << i << " thread done with the task!" << std::endl;
                     }
                 }
@@ -55,13 +58,46 @@ class SimplePool
         }
     }
 
-    void add_task(std::function<void()> f)
+    // The 'enqueue' method is implemented as a template function to enable perfect forwarding (universal references + std::forward).
+    // Previously, regardless of what was passed to the function, a copy operations was always performed - even for rvalues. Inefficient.
+    // Now we achieved better scalability : rvalues are moved, lvalues are copied.
+
+    // - Free functions are lvalue;
+    // - Functors are lvalues;
+    // - Lambdas (temporary) are rvalues (prvalue);
+    // - The results od std::bind() are rvalues;
+    template <typename F>
+    std::future<void> enqueue(F&& f)
     {
+        using task_type = std::packaged_task<void()>;
+        
+        // enqueue(lambda1) --> F&& = SomeLambda&& --> std::forward<SomeLambda&&>(lambda1) ~ std::move(lambda1)
+        // enqueue(functor1) --> F&& = SomeFunctor& && --> reference collapsing --> SomeFunctor& --> std::forward<SomeFunctor&>(functor1) ~ no-op. Copy.
+
+        // The shared_ptr<task_type> (task) remains alive as long as at least one of the following is true:
+        //   - the task is still present in the queue (has not yet been picked up and executed by a worker thread)
+        //   - or some std::future still holds a reference to it (nobody has called .get() / .wait() yet, or the future object is still alive)
+        //
+        // In other words: the task object is destroyed only after both
+        //   - it has been executed (removed from queue)
+        //   - and all futures associated with it have been destroyed or completed (.get()/.wait() called)
+        std::shared_ptr<task_type> task_ptr = std::make_shared<task_type>(std::forward<F>(f));
+        std::future<void> future = task_ptr->get_future();
+
         {
             std::lock_guard<std::mutex> lock_g(mtx);
-            queue_tasks.emplace(f);
+            if (done)
+            {
+                throw std::runtime_error{"Enqueue operation on stopped pool"};
+            }
+            queue_tasks.emplace([task_ptr]
+            {
+                (*task_ptr)();
+            });
         }
+
         cv.notify_one();
+        return future;
     }
 
     ~SimplePool()
@@ -90,17 +126,16 @@ int main()
     // Because the predicate is false, each thread goes into a sleep state.
     std::this_thread::sleep_for(std::chrono::seconds(3));
 
-    pool.add_task([](){std::cout << "Errand1" << std::endl;});
     // Once a task is added, an arbitrary thread wakes up and evaluates the predicate. 
     // Since it is true, after releasing the mutex the thread performs the task.
     // After it returns to the while loop to check the predicate again.
-    pool.add_task([](){std::cout << "Errand2" << std::endl;});
-    pool.add_task([](){std::cout << "Errand3" << std::endl;});
-    pool.add_task([](){std::cout << "Errand4" << std::endl;});
-    pool.add_task([](){std::cout << "Errand5" << std::endl;});
-    pool.add_task([](){std::cout << "Errand6" << std::endl;});
-    pool.add_task([](){std::cout << "Errand7" << std::endl;});
-    pool.add_task([](){std::cout << "Errand8" << std::endl;});
+    for (int i = 1; i <= 8; ++i) {
+        pool.enqueue([i] {
+            std::this_thread::sleep_for(std::chrono::milliseconds(200 * i));
+            std::lock_guard<std::mutex> lock_g(global_cout_mutex);
+            std::cout << "Errand " << i << " done\n";
+        });
+    }
 
     return 0;
 }
